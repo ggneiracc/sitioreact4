c45f11d90663dbdb169a5ca8f2ccc2817515fff6
ggneiracc68 <ggneiracc68@gmail.com>
2021-07-14 18:33:51 -0500

    Versión 1. Solo tiene un mensaje en el componente

---
+++ src/components/C01componente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const C01componente = () => {
+    return (
+        <div>
+            Componente 1. Hola mundo desde React
+        </div>
+    )
+}
+
+export default C01componente
