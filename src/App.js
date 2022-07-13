ef1e2bc75433765d669117f8c76af92ef40ff5d5
ggneiracc <ggneiracc68@gmail.com>
2022-07-12 23:50:22 -0500

    Versión 1. Sitio React, muestrade publicación

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Sitio Web para demostrar subida del sitio a GitHub
         </p>
         <a
           className="App-link"
